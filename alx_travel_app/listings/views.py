797121486beea7f821a61f22372e7c484bebb677
Precious <banigoprecious@yahoo.com>
2025-02-08 00:54:45 +0100

    alx_travel_app_0x02

---
+++ alx_travel_app/listings/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Listing
+from .serializers import ListingSerializer
+from .models import Booking
+from .serializers import BookingSerializer
+
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
